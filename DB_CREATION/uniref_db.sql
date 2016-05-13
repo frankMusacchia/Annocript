30239677cfa8c98c0755e2c7179700338de5bb3c
Francesco <francesco [dot] musacchia [at] szn [dot] it>
2016-05-13 21:44:56 +0200

    Sat_13_may

---
+++ DB_CREATION/uniref_db.sql
@@ -0,0 +1,233 @@
+/*
+*   dbCreation.sql
+*   author: Francesco Musacchia
+*   summary: the tables for UNIPROT_KB database
+* ################################
+* BE CAREFUL!!!!!
+* When you update this script to change tables in the database,
+* you must also change the version here and the variable $dbTablesVersion
+* in annocript.pl
+* #################################
+*/
+
+
+/*FOREIGN KEYS HAVE TO BE WRITTEN EVERYTIME AT THE END OF
+THE TABLE DEFINITION
+PRAGMA foreign_keys = ON;/*In SQLITE foreign_keys are de-activated by default*/
+
+/*Descriptions of the proteins*/   
+CREATE TABLE unirefDescType (
+    id MEDIUMINT unsigned PRIMARY KEY,
+    descr MEDIUMTEXT not null);
+
+/*Taxonomy*/
+CREATE TABLE taxType (
+    id MEDIUMINT unsigned PRIMARY KEY,
+    descr TEXT not null);
+    
+/*This table will contain the GO ids with their descriptions*/    
+CREATE TABLE goTable (
+    id INT unsigned PRIMARY KEY,
+    go_id  varchar(50) not null,
+    definition MEDIUMTEXT,
+    division ENUM('C','F','P'),
+    unique (go_id) );
+
+/*Domains with descriptions*/ 
+CREATE TABLE cdTable (
+    cd_id INT unsigned PRIMARY KEY,
+    genericId  varchar(50) not null,
+    cdName  varchar(50) not null,
+    cdShortName varchar(50) not null,
+    cdDescription MEDIUMTEXT,
+    pssmLength INT unsigned,
+    unique (cdName) );
+    
+            
+/*A table with the mapping from the domains to their GO ids*/   
+CREATE TABLE cdMappingsTable (
+    cd_id INT unsigned not null,
+    go_ids MEDIUMTEXT,
+    FOREIGN KEY (cd_id) REFERENCES cdTable(cd_id),
+    PRIMARY KEY (cd_id)
+    );
+        
+/*This table will contain the EC Enzyme ids with their descriptions*/         
+CREATE TABLE enzymeTable (
+	id INT unsigned PRIMARY KEY, 
+	enz_id varchar(50) not null, 
+	description mediumtext, 
+	unique (enz_id));
+
+/*This table will contain the Pathways ids with their descriptions*/        
+CREATE TABLE pathwaysTable (
+	id INT unsigned PRIMARY KEY, 
+	description mediumtext, 
+	unique (id));
+
+/*This is a view that permits to access the GO terms ids of a 
+given doomain using its ID*/   
+create view cdViewGO as
+    select c.cdName as cdName, m.go_ids as go_ids
+        FROM  cdMappingsTable m LEFT JOIN cdTable c ON  m.cd_id = c.cd_id;
+        
+/*This is the main table with the proteins and all the ids referred to descriptions in other
+tables*/                                       
+CREATE TABLE uniref(
+    id INT unsigned not null,
+            seqId varchar (50) not null,
+            descId MEDIUMINT unsigned not null,
+            n MEDIUMINT unsigned not null,
+            taxId MEDIUMINT  unsigned not null,
+            repId varchar (50) not null,
+            FOREIGN KEY (descId) REFERENCES unirefDescType(id),
+            FOREIGN KEY (taxId) REFERENCES taxType(id),
+            /*FOREIGN KEY (id) REFERENCES mappingsTable(uniref_id),*/
+            unique(seqId),
+            primary key (id)
+            );          
+            
+CREATE TABLE unirefMappingsTable (
+    uniref_id INT unsigned not null,
+    go_ids MEDIUMTEXT,
+    /*enzyme_ids MEDIUMTEXT,
+    pw_ids MEDIUMTEXT,*/
+    FOREIGN KEY (uniref_id) REFERENCES uniref(id),
+    PRIMARY KEY (uniref_id)
+    );
+
+    
+/*This is a composite view formed by two views (A, B) that permits to access the taxonomy, description and go ids of a 
+given protein using its uniref ID*/
+create view unirefViewSeqA as
+    select u.id as uniId, u.seqId as seqId, g.go_ids as go_ids
+        FROM  uniref u LEFT JOIN unirefMappingsTable g  ON  g.uniref_id = u.id;
+           
+create view unirefViewSeqB as
+    select u.id as uniId2, tax.descr as taxType, d.descr as description
+	FROM (uniref u INNER JOIN taxType tax ON u.taxId=tax.id)
+                        INNER JOIN  unirefDescType d ON u.descId=d.id;
+                        
+create view unirefViewSeq as
+    select uniId, seqId, taxType, description, go_ids
+        from unirefViewSeqA LEFT JOIN unirefViewSeqB ON uniId=uniId2;
+        
+        
+/*This is a view that permits to access the enzyme ids of a 
+given protein using its uniprot ID       
+create view unirefViewEnzyme as
+    select u.seqId as seqId, m.enzyme_ids as enzyme_ids
+        FROM  unirefMappingsTable m LEFT JOIN uniref u ON  m.uniref_id = u.id;*/ 
+  
+
+
+
+/****FOR SWISSPROT****/
+
+/*This table contains indication of the database where the proteins have been taken (Swiss-Prot or Trembl)*/
+CREATE TABLE dbType (
+    id TINYINT unsigned PRIMARY KEY, 
+    type varchar(20) not null,
+    unique (type) );
+
+/*Descriptions of the proteins*/    
+CREATE TABLE uniprotkbDescType (
+    id MEDIUMINT unsigned PRIMARY KEY,
+    type MEDIUMTEXT not null);
+
+/*Organisms*/
+CREATE TABLE OSType (
+    id MEDIUMINT unsigned PRIMARY KEY,
+    type TEXT not null);
+    
+CREATE TABLE GNType (
+    id MEDIUMINT unsigned PRIMARY KEY,
+    type MEDIUMTEXT  not null);
+    
+CREATE TABLE PEType (
+    id TINYINT unsigned PRIMARY KEY,
+    type  varchar(10) not null,
+    unique (type));
+    
+CREATE TABLE SVType (
+    id TINYINT unsigned PRIMARY KEY,
+    type  varchar(10) not null,
+    unique (type));
+
+   
+
+/*This is the main table with the proteins and all the ids referred to descriptions in other
+tables*/  
+CREATE TABLE uniprotkb (
+            id INT unsigned PRIMARY KEY,
+            seqId varchar (20) not null,
+            dbId TINYINT unsigned not null,
+            descId MEDIUMINT unsigned not null,
+            OSId MEDIUMINT  unsigned not null,
+            GNId MEDIUMINT unsigned not null,
+            PEId TINYINT  unsigned not null,
+            SVId TINYINT unsigned not null,
+            FOREIGN KEY(dbId) REFERENCES dbType(id),
+            FOREIGN KEY(descId) REFERENCES uniprotkbDescType(id),
+            FOREIGN KEY(OSId) REFERENCES OSType(id),
+            FOREIGN KEY(GNId) REFERENCES GNType(id),
+            FOREIGN KEY(PEId) REFERENCES PEType(id),
+            FOREIGN KEY(SVId) REFERENCES SVType(id),
+            /*FOREIGN KEY(id) REFERENCES uniprotkbMappingsTable(uniprot_id),*/
+            unique(seqId)
+            );
+            
+/*A table with the mapping from the proteins to their GO and ENZYME ids*/   
+CREATE TABLE uniprotkbMappingsTable (
+    uniprot_id INT unsigned not null,
+    go_ids MEDIUMTEXT,
+    enzyme_ids MEDIUMTEXT,
+    pw_ids MEDIUMTEXT,
+    FOREIGN KEY (uniprot_id) REFERENCES uniprotkb(id),
+    PRIMARY KEY (uniprot_id)
+    );
+  
+/*A table that says if a table has been created or not. If yes it should be there.*/                              
+CREATE TABLE goodTables (
+    id int NOT NULL AUTO_INCREMENT,
+    tableName varchar(50) not null,
+    unique (tableName),
+    PRIMARY KEY (ID) 
+  );
+ 
+/*A table containing info about the database*/                              
+CREATE TABLE infoTable (
+    dbName varchar (20),
+    dbRelDate varchar (20),
+    annocriptVer varchar (20),
+    dbTablesVersion varchar (20)
+  );
+       
+ /*This is a composite view formed by two views (A, B) that permits to access the taxonomy, description and go ids of a 
+given protein using its uniprot ID*/
+create view uniprotkbViewSeqA as
+    select u.id as uniprotId, u.seqId as seqId, g.go_ids as go_ids
+        FROM  uniprotkb u LEFT JOIN uniprotkbMappingsTable g  ON  g.uniprot_id = u.id;
+           
+create view uniprotkbViewSeqB as
+    select u.id as uniprotId2, os.type as OSType, d.type as description
+	FROM (uniprotkb u INNER JOIN OSType os ON u.OSId=os.id)
+                        INNER JOIN  uniprotkbDescType d ON u.descId=d.id;
+                        
+create view uniprotkbViewSeq as
+    select uniprotId, seqId, OSType, description, go_ids
+        from uniprotkbViewSeqA LEFT JOIN uniprotkbViewSeqB ON uniprotId=uniprotId2;
+        
+/*This is a view that permits to access the enzyme ids of a 
+given protein using its uniprot ID*/   
+create view uniprotkbViewEnzyme as
+    select u.seqId as seqId, m.enzyme_ids as enzyme_ids
+        FROM  uniprotkbMappingsTable m LEFT JOIN uniprotkb u ON  m.uniprot_id = u.id;
+
+
+/*This is a view that permits to access the pathways ids of a 
+given protein using its uniprot ID*/   
+create view uniprotkbViewPathways as
+    select u.seqId as seqId, m.pw_ids as pw_ids
+        FROM  uniprotkbMappingsTable m LEFT JOIN uniprotkb u ON  m.uniprot_id = u.id;
+
---
+++ README.md
@@ -0,0 +1,131 @@
+#*Annocript 1.1.2*
+--------------------------------
+
+*Annocript* is a pipeline for the annotation of *de-novo* generated transcriptomes. It executes blast
+ analysis with UniProt, NCBI Conserved Domain Database and Nucleotide division adding also annotations 
+from Gene Ontology, the Enzyme Commission and UniPathways. *Annocript* also gives information about the longest ORF
+and the non-coding potential using external software. 
+*Annocript* is also capable to identify putative long non-coding RNAs by using an heuristic based on homology and 
+sequence features.
+
+The inpatient may go directly at the [INSTALL](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/INSTALL.md) page. Then, you should visit the tutorial at [TUTORIAL](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/TUTORIAL.md) to learn how to use *Annocript*.
+
+##Mission
+
+*Annocript* is a highly configurable tool to annotate and analyze transcriptomes. After the transcriptome annotation you may want to run an expression analysis. R Scripts to perform differential expression analysis are already present in the section Annocript_utils. We are currently using edgeR (Robinson *et al*, 2010) in a script that starts from the raw counts table. In the same section you can also find a useful Python script for intelligent search of specific words inside user-selectable columns of the Annocript output. Other useful plots and analyses are in work in progress and will be made available as soon as possible.
+
+Since Annocript is not coming with a double-click installation you should dedicate a bit of time the first time you use it. 
+
+*But don't run away!*
+
+This is a very simple guide, it is big only because everything is explained step-by-step but we promise that,
+ after you installed and ran once, you will do it very rapidly the following times and you will never stop using it!
+We strongly suggest to keep open this guide while you are installing and running *Annocript* for the first time.
+
+Examples of whole transcriptome annotations made by Annocript can be downloaded from http://bit.ly/15vnALW. 
+
+Please cite: *Musacchia F, Basu S, Petrosino G, Salvemini M, Sanges R. Annocript: a flexible pipeline for the annotation of transcriptomes which can also identify putative long non-coding RNA*
+
+##Installation and tutorial
+
+To start using it you must follow instructions in the **GUIDE** folder.
+Below is a simple introduction of what you will find there and how to find solutions to possible problems.
+- *Annocript* has some specific purposes. To figure out what they are, please read the [INTRO](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/INTRO.md) file;
+- Since *Annocript* is a pipeline, it uses some software that you have to download and install. Please follow 
+the instructions on how to do it in the [INSTALL](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/INSTALL.md) file. There you will also learn how to install *Annocript*;
+- Every software has its thorns! To understand how to run *Annocript* the first time please read the [TUTORIAL](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/TUTORIAL.md) file;
+- When you will be an advanced user you will want to use *Annocript* in a faster and smarter way. [ADVANCED_USAGE](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/ADVANCED_USAGE.md)
+explains how to begin to do this. Here, an HINT and TIPS section contains a lot of useful information on how to
+ perform advanced operations;
+- [FAQ](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/FAQ.md) contains Frequently Asked Questions (refer to the FAQ whenever you have errors that block *Annocript*);
+- [OUTPUT](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/OUTPUT.md) contains a list of all the output that *Annocript* gives, the organization of folders and 
+of the configuration files; It also explains what are the field of the tabular output of *Annocript*.
+- [HISTORY](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/HISTORY.md) is the history of changes to *Annocript* from it's creation to the current release.
+
+
+Please enjoy this product and send us questions and suggestions!
+
+##System Requirements
+
+To work, *Annocript* needs:
+- MySQL database server 
+- You also need a MySQL client.
+- Perl  
+- BioPerl 
+- Python 
+- R 
+- Blast+
+- Portrait
+- dna2pep
+
+Check for the tested versions in the [INSTALL](https://github.com/frankMusacchia/Annocript/blob/master/GUIDE/INSTALL.md) page.
+
+
+##*Annocript* times and space
+
+Any user will wonder how much time and space *Annocript* needs. We can say approximate times and sure space dimensions.
+*Annocript* execution time depends strongly from what modules the user is executing and of course from the speed and memory of your machine. Here we only show the most important processes that are always taking the biggest part of the overall computational time.
+
+
+###Database Creation
+
+The database creation needs to be done only once but it depends on what proteins db you are creating (Uniref or Uniprot_kb).
+If you decide to use Uniprot_kb, *Annocript* will fill an annotation table with both the BLAST results from Swiss-Prot and Trembl
+while if you are using Uniref, both Swiss-Prot and Uniref result will be shown.
+The database comprise also information about domains, GO, enzymes and pathways. Furthermore, databases to use for BLAST will 
+be downloaded also (see the **INTRO** file for more information about this step).
+
+Consider as a suggestion that, if your machine is not very recent and powerful you can consider to install Uniref database (Uniref50
+takes the lowest time and space). Otherwise Uniprot_kb is a good choice.
+
+####Times
+
+We took times for the creation of the database from 3 different machines with different cores and RAM memory: 
+- Machine with 24 core and 96 GBRAM 
+  - Uniprot_kb_2014_03 - 7 hours
+  - Uniref_2014_04 - 3 hours
+- Machine with 24 core and 24 GBRAM 
+  - Uniref 2013_01 - 9 hours 
+  - Uniprot_kb 2014_03 - 21 hours 
+- Machine with 8 core and 8 GBRAM
+  - Uniref 2014_05 - 38 hours
+
+The speed of the overall process depends greatly from the time required for the download of the huge databases. Here the times were 
+taken by using approximately 1MB/sec in download but depending from the speed of you connection the download process may 
+take few hours more.
+
+
+####Space
+
+The space needed to build the MySQL database is approximately: 
+- UNIREF 2014_04: 52.61GB (50.1GB for files; 2.4 GB MySQL db) 
+- UNIPROTKB 2014_04: 92.6GB (85.3GB for files; 7.3 GB MySQL db)
+
+Newer protein databases will slightly increase the necessary space according to their growth in term of proteins numbers.
+You can save some space by choosing, during the configuration, to remove the downloaded .zip files.
+- zip files in Uniref: 8.5GB
+- zip files in Uniprotkb: 14.5GB
+
+###Programs Execution
+
+Programs execution strongly depends from the transcriptome and the database we are using. The number of sequences but mostly 
+their mean length.
+We took the times of the blastx execution (the most expensive process) against the Uniref90 database with our default parameters 
+(which you will find in the config_user.txt) on a machine with 24 threads (2 Intel(R) Xeon(R) X5660 at 2.80GHz and 96GB RAM). 
+This machine permitted the multi-threaded run of blast and rpsblast. We always used 20 core. The followings are the times when depending from mean length of sequences.
+
+####Times
+
+It needed about: 
+- 6 hrs for 11351 transcripts of mean length 951 nucleotides; 
+- 13 hrs for 30346 transcrips of mean length 1245 nucleotides;
+- 20 hrs for 17776 transcripts of mean length 1478 nucleotides;
+- 23 hrs for 30339 transcripts of mean length 1427 nucleotides;
+- 46 hrs for 98174 transcripts of mean length 965 nucleotides;
+- 72 hrs for 64388 transcripts of mean length 2125 nucleotides.
+
+Depending by the powerful of you machine, the BLASTx and the rpsBLAST execution may take few hours more. 
+
+---------------------------------------
+
+If you get some error during the installation or the running of *Annocript* please see the FAQ page of the user guide or ask on the google group: https://groups.google.com/forum/#!forum/annocript
